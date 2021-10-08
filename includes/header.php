579bbf0aa7fd92ee54249fe3731d7a61f58eec3d
AlexanderHenaoC <ahenao37@misena.edu.co>
2021-10-07 23:08:43 -0500

    Añado dir includes con footer y header

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/stylely.css">
+    <link rel="shortcut icon" href="/templates/images/logo_small_icon_only.png" type="image/png">
+    <title>FixCar</title>
+</head>
+
+<body>
+    <header>
+        <div class="cont-header">
+            <div class="cont-title">
+                <img src="/templates/images/logo_small_icon_only.png" alt="">
+                <h1><a href="home.html">FixCar</a></h1>
+            </div>
+            <div class="cont-nav">
+                <nav>
+                    <ul>
+                        <li><a href="#">Quiénes somos</a></li>
+                        <li><a href="#">¿Cómo funciona?</a></li>
+                        <li><a href="/templates/login.html">Iniciar sesión</a></li>
+                        <li><a href="#">Registrarse</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main class="main">
---
+++ templates/login.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/stylely.css">
+    <link rel="stylesheet" href="/styles/login.css">
+    <link rel="shortcut icon" href="/templates/images/logo_small_icon_only_opt.png" type="image/png">
+    <title>FixCar</title>
+</head>
+
+<body>
+    <header>
+        <div class="cont-header">
+            <div class="cont-title">
+                <img src="/templates/images/logo_small_icon_only_opt.png" alt="">
+                <h1><a href="home.html">FixCar</a></h1>
+            </div>
+            <div class="cont-nav">
+                <nav>
+                    <ul>
+                        <li><a href="#">Quiénes somos</a></li>
+                        <li><a href="#">¿Cómo funciona?</a></li>
+                        <li><a href="/templates/login.html">Iniciar sesión</a></li>
+                        <li><a href="#">Registrarse</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main class="main">
+        <div class="cont-main">
+            <div>
+                <div class="cont-log-img">
+                    <img class="i-lg it" src="/templates/images/s3_opt.jpg" alt="">
+                    <img class="i-lg ic" src="/templates/images/sc_opt.jpg" alt="">
+                    <img class="i-lg ici" src="/templates/images/s5_opt.jpg" alt="">
+                    <img class="i-lg iu" src="/templates/images/su_opt.jpg" alt="">
+                    <img class="i-lg id" src="/templates/images/sd_opt.jpg" alt="">
+                </div>
+                <div class="frm-log">
+                    <h4>Ingrese aquí</h4>
+                    <input type="text" name="usuario" placeholder="Ingrese correo aquí">
+                    <input type="password" name="contraseña" placeholder="Ingrese su contraseña aquí">
+                    <button class="btn"><a href="#">Ingresar</a></button>
+                    <p class="ntuc">No tienes una cuenta?<br><br>
+                        Registrate <a href="#">aquí</a></p>
+                    <!-- <p class="liw">Ingrese con</p> -->
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="cont-ftr">
+            <div class="cont-ftr-lft p-t">
+                <div class="ftr-lft-lg">
+                    <!-- logo -->
+                    <a href="home.html"><img src="/templates/images/logo_small_icon_only_opt.png" alt=""></a>
+                </div>
+                <div class="ftr-lft-nm">
+                    <!-- nombre -->
+                    <a href="home.html">Fix Car</a>
+                </div>
+                <div class="ftr-lft-rs">
+                    <!-- redes sociales -->
+                    <div class="ftr-rs c-img">
+                        <!-- facebook -->
+                        <a href="#"><img src="/templates/images/facebook_opt.png" alt=""></a>
+                    </div>
+                    <div class="ftr-rs c-img">
+                        <!-- twitter -->
+                        <a href="#"><img src="/templates/images/twitter_opt.png" alt=""></a>
+                    </div>
+                    <div class="ftr-rs c-inst">
+                        <!-- instagram -->
+                        <a href="#"><img src="/templates/images/instagram_opt.png" alt=""></a>
+                    </div>
+                    <div class="ftr-rs c-img">
+                        <!-- ln -->
+                        <a href="#"><img src="/templates/images/linkedin_opt.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="cont-ftr-cnt p-t">
+                <div class="ftr-cnt-btc">
+                    <button>Agenda tu cita</button>
+                </div>
+                <div class="ftr-cnt-cr">
+                    <a href="#">Aquí_va_el_correo@correo.com</a>
+                </div>
+                <div class="ftr-cnt-tl">
+                    <a href="#">311 1111111</a>
+                </div>
+            </div>
+            <div class="cont-ftr-rgt p-t">
+                <div class="ftr-rgt-icn c-img-r">
+                    <img src="/templates/images/check_opt.png" alt="">
+                </div>
+                <div class="ftr-rgt-srv">
+                    <a href="#">Servicios</a>
+                </div>
+                <div class="ftr-rgt-icn c-img-r">
+                    <img src="/templates/images/check_opt.png" alt="">
+                </div>
+                <div class="ftr-rgt-qs">
+                    <a href="#">Quiénes somos</a>
+                </div>
+                <div class="ftr-rgt-icn c-img-r">
+                    <img src="/templates/images/check_opt.png" alt="">
+                </div>
+                <div class="ftr-rgt-cf">
+                    <a href="#">Cómo funciona</a>
+                </div>
+            </div>
+            <div class="cont-ftr-md p-t">
+                <div class="ftr-md-pqr ftr-md">
+                    <a href="#">Preguntas frecuentes</a>
+                </div>
+                <div class="ftr-md-pf ftr-md">
+                    <a href="#">Peticiones quejas y reclamos</a>
+                </div>
+                <div class="ftr-md-il ftr-md">
+                    <a href="#">Información Legal</a>
+                </div>
+                <div class="ftr-md-pd ftr-md">
+                    <a href="#">Política de datos</a>
+                </div>
+                <div class="ftr-md-c ftr-md">
+                    <a href="#">Política de garantías</a>
+                </div>
+                <div class="ftr-md-pg ftr-md">
+                    <a href="#">Condiciones del servicio</a>
+                </div>
+            </div>
+            <div class="cont-ftr-btm p-t">
+                <p>SERVICIO NACIONAL DE APRENDIZAJE SENA - CENIGRAF |
+                    TECNICO EN PROGRAMACIÓN DE SOFTWARE FICHA 2278730 |
+                    2021 FIXCAR</p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
