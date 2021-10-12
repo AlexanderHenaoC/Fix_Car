f5e894d28a5a661161b1cd4f2816f92bdd7b9b64
AlexanderHenaoC <ahenao37@misena.edu.co>
2021-10-11 20:11:04 -0500

    No hice cambios

--- templates/login.php
+++ templates/login.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/stylely.css">
     <link rel="stylesheet" href="../styles/home.css">
     <link rel="stylesheet" href="../styles/login.css">
+    <link rel="stylesheet" href="../styles/stylely.css">
     <link rel="shortcut icon" href="../images/logo_small_icon_only.png" type="image/png">
     <title>Ingreso</title>
 </head>
